8e5b3630387bfb5e2c02b3a5873721d6003b19d5
Merey Zholdas <merey.zholdas@deliveryhero.com>
2020-06-25 20:33:11 +0200

    moved to main component

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,5 +1,36 @@
 import React from "react";
+import { ShoppingList } from "../ShoppingList";
+import { NewItemForm } from "../NewItemForm";
 
 export const Main = () => {
-  return <main>Main</main>;
+  const [shoppingList, setShoppingList] = React.useState([
+    {
+      id: 0,
+      title: "Banana",
+      done: true,
+    },
+    {
+      id: 1,
+      title: "Apple",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "Milk",
+      done: false,
+    },
+  ]);
+
+  const handleAddNewItem = (newShoppingItem) => {
+    // ... - spread operator, rasformirovyvaet massiv
+    setShoppingList([...shoppingList, newShoppingItem]);
+  };
+
+  return (
+    <main>
+      <ShoppingList shoppingList={shoppingList} />
+      <hr />
+      <NewItemForm newId={shoppingList.length} addNewItem={handleAddNewItem} />
+    </main>
+  );
 };
