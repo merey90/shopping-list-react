126d5b05cacd9dab7f1fd35206b4d17570174692
Merey Zholdas <merey.zholdas@deliveryhero.com>
2020-07-16 22:41:17 +0200

    styling the header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  Box,
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Toolbar, Typography, Button, Box } from '@material-ui/core';
+
+import './styles.css';
 
 export const Header = () => (
   <AppBar position="static">
     <Toolbar>
-      <IconButton edge="start" color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography component={Link} variant="h6" to="/">
+      <Typography
+        color="inherit"
+        className="header-title"
+        component={Link}
+        variant="h6"
+        to="/"
+      >
         Shopping List
       </Typography>
       <Box display="flex" justifyContent="flex-end" flexGrow="1">
