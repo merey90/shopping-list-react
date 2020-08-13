926890d61cf5eaea84a9bb966d7f5ee924a0a007
Merey Zholdas <merey.zholdas@deliveryhero.com>
2020-08-13 20:58:15 +0200

    delete items

--- src/ShoppingList/index.js
+++ src/ShoppingList/index.js
@@ -1,35 +1,51 @@
 import React from 'react';
-import { FormControlLabel, Checkbox } from '@material-ui/core';
+import {
+  FormControlLabel,
+  Checkbox,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import './styles.css';
 
-export const ShoppingList = ({ showAll, shoppingList, changeItem }) => {
-  const handleChange = (id) => {
-    changeItem(id);
-  };
+export const ShoppingList = ({
+  showAll,
+  shoppingList,
+  changeItem,
+  deleteItem,
+}) => (
+  <List>
+    {shoppingList.map((shopping) => {
+      if (!showAll && shopping.done) {
+        return null;
+      }
 
-  return (
-    <ul className="shopping-list">
-      {shoppingList.map((shopping) => {
-        if (!showAll && shopping.done) {
-          return null;
-        }
-
-        return (
-          <li key={shopping._id}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={shopping.done}
-                  onChange={() => handleChange(shopping._id)}
-                  color="primary"
-                />
-              }
-              label={shopping.title}
-            />
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+      return (
+        <ListItem dense key={shopping._id}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={shopping.done}
+                onChange={() => changeItem(shopping._id)}
+                color="primary"
+              />
+            }
+            label={shopping.title}
+          />
+          <ListItemSecondaryAction>
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => deleteItem(shopping._id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      );
+    })}
+  </List>
+);
